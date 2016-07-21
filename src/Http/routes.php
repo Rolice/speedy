f30838d80404df6412716b43d28ac325d341018f
Rolice <l.gardev@intellisys.org>
2016-07-21 13:56:49 +0300

    Fixes on package routing.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,7 +1,8 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'speedy','middleware' => 'Rolice\Speedy\Http\Middleware\Speedy', function () {
+Route::group(['prefix' => 'speedy','middleware' => 'Rolice\Speedy\Http\Middleware\Speedy'], function () {
 
     Route::post('test', ['as' => 'speedy.test', 'uses' => 'Rolice\Speedy\Http\Controllers\SpeedyController@test']);
 
-}]);
+});
