cad85ce8e2ede9a323a7497e184dcb801cd8a427
Rolice <l.gardev@intellisys.org>
2016-07-28 17:40:05 +0300

    Added option to search offices by settment name, not only ID.

--- src/Http/Controllers/OfficeController.php
+++ src/Http/Controllers/OfficeController.php
@@ -5,6 +5,7 @@ use Input;
 use Rolice\Speedy\Components\Client;
 use Rolice\Speedy\Components\Language;
 use Rolice\Speedy\Components\OfficeEx;
+use Rolice\Speedy\Components\Site;
 use Rolice\Speedy\Exceptions\SpeedyException;
 use Rolice\Speedy\Speedy;
 
@@ -19,13 +20,6 @@ class OfficeController extends Controller
 
     public function autocomplete()
     {
-        $settlement = (int)Input::get('settlement');
-        $name = htmlentities(Input::get('query'), ENT_QUOTES, 'UTF-8', false);
-
-        if (0 >= $settlement) {
-            return ['results' => [], 'more' => false];
-        }
-
         $client = Client::createFromArray(Input::all());
 
         if (!$client) {
@@ -38,10 +32,28 @@ class OfficeController extends Controller
         $speedy = app('speedy');
         $speedy->user($client);
 
+        $settlement = Input::get('settlement');
+
+        if ((string)(int)$settlement != $settlement) {
+            $site = Site::find($settlement);
+
+            if ($site instanceof Site) {
+                $settlement = $site->id;
+            }
+        }
+
+        $settlement = (int) $settlement;
+
+        $name = htmlentities(Input::get('query'), ENT_QUOTES, 'UTF-8', false);
+
+        if (0 >= $settlement) {
+            return ['results' => [], 'more' => false];
+        }
+
         $offices = $speedy->listOfficesEx($settlement, Language::create(), $name);
 
-        if (!isset($offices->return)) {
-            throw new SpeedyException('Error while searching for Speedy offices.');
+        if (!$offices || !isset($offices->return)) {
+            return ['results' => [], 'more' => false];
         }
 
         $result = [];
