bccc87a84c9fbba7d178cbf2ab60534c85dded12
Rolice <l.gardev@intellisys.org>
2016-08-15 12:28:08 +0300

    Added generic trnaslation files.

---
+++ resources/lang/en/speedy.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'sender' => [
+            'name' => 'Sender name',
+            'phone' => 'Sender phone',
+            'settlement' => 'Sender settlement',
+            'pickup' => 'The sender will pick up from',
+            'street' => 'Street',
+            'street_num' => 'Street Number',
+            'street_vh' => 'Entrance',
+            'office' => 'Office',
+        ],
+
+        'receiver' => [
+            'name' => 'Receiver name',
+            'phone' => 'Receiver phone',
+            'settlement' => 'Receiver settlement',
+            'pickup' => 'The receiver will pick it up from',
+            'street' => 'Street',
+            'office' => 'Office',
+        ],
+
+        'shipment' => [
+            'num' => 'Shipment number',
+            'type' => 'Type of shipment',
+            'description' => 'Choose the type of the shipment',
+            'count' => 'Package quantity',
+            'weight' => 'Weight of shipment',
+        ],
+
+        'services' => [
+            'date' => 'Data',
+            'service' => 'Service',
+        ],
+    ],
+];
